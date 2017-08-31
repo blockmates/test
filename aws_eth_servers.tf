9caa44878ad99241ab9d8428df8497e26b5d9d79
blockmates <31468531+blockmates@users.noreply.github.com>
2017-08-31 19:36:43 +0200

    Create aws_eth_servers.tf

---
+++ aws_eth_servers.tf
@@ -0,0 +1,13 @@
+resource "aws_lightsail_instance" "Ethereum_Master" {
+  name              = "master"
+  availability_zone = "eu-central-1a"
+  blueprint_id      = "ubuntu_16_04"
+  bundle_id         = "nano_1_0"
+} 
+
+resource "aws_lightsail_instance" "Ethereum_Slave" {
+  name              = "slave"
+  availability_zone = "eu-central-1a"
+  blueprint_id      = "ubuntu_16_04"
+  bundle_id         = "nano_1_0"
+} 
