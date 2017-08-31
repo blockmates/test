c459cefb8a3004f6a190ec75598907e3a27271d3
blockmates <31468531+blockmates@users.noreply.github.com>
2017-08-31 19:57:22 +0200

    Update aws_eth_servers.tf

--- aws_eth_servers.tf
+++ aws_eth_servers.tf
@@ -5,8 +5,15 @@ resource "aws_lightsail_instance" "Ethereum_Master" {
   bundle_id         = "nano_1_0"
 } 
 
-resource "aws_lightsail_instance" "Ethereum_Slave" {
-  name              = "slave"
+resource "aws_lightsail_instance" "Ethereum_Slave1" {
+  name              = "slave1"
+  availability_zone = "eu-central-1a"
+  blueprint_id      = "ubuntu_16_04"
+  bundle_id         = "nano_1_0"
+} 
+
+resource "aws_lightsail_instance" "Ethereum_Slave2" {
+  name              = "slave2"
   availability_zone = "eu-central-1a"
   blueprint_id      = "ubuntu_16_04"
   bundle_id         = "nano_1_0"
