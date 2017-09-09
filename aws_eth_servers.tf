38ba41b19932e1a8b3b7a4fbc732d3a04e85f3ea
blockmates <31468531+blockmates@users.noreply.github.com>
2017-09-09 22:45:47 +0200

    Update aws_eth_servers.tf

--- aws_eth_servers.tf
+++ aws_eth_servers.tf
@@ -22,12 +22,3 @@ resource "aws_lightsail_instance" "Ethereum_Master" {
     ]
   }
 } 
-
-resource "aws_lightsail_instance" "Ethereum_Slave1" {
-  name              = "slave1"
-  availability_zone = "eu-central-1a"
-  blueprint_id      = "ubuntu_16_04"
-  bundle_id         = "nano_1_0"
-} 
-
-# /mnt/c/Users/Jean-PaulvanderHam/OneDrive\ -\ j-enable/Blockchain/setup.sh
